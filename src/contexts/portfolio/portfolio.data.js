26f800a24c48ad9aa2c296e1151c3af429e5c97b
Alfred Asare <alfredasare101@gmail.com>
2020-06-16 08:09:03 +0000

    copied from repo

--- src/contexts/portfolio/portfolio.data.js
+++ src/contexts/portfolio/portfolio.data.js
@@ -3,17 +3,34 @@ import uuid from 'react-uuid';
 export const PORTFOLIO_DATA = [
     {
         id: uuid(),
-        index: 1,
-        name: "Project 1",
-        projectCategory: "UI / UX",
-        urlRep: 'project1.com',
-        projectInfo: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur cum eius pariatur quibusdam quos reiciendis rerum " +
-            "sed ullam velit voluptate? Repellendus, voluptates, voluptatum! Accusamus dicta, ducimus iusto perspiciatis quae temporibus?",
+        index: 0,
+        projectName: 'Chop-flix',
+        logo: require('../../assets/images/chop_flix_logo.png'),
         projectResources: [
             {
                 id: uuid(),
                 type: 'pic',
+                image: require('../../assets/images/chop_flix.jpg'),
+                websiteLink: 'https://chop-flix.netlify.app/',
+            },
+            {
+                id: uuid(),
+                type: 'pic',
                 url: ``
+            }
+        ],
+    },
+    {
+        id: uuid(),
+        index: 1,
+        projectName: 'Drvmroll',
+        logo: require('../../assets/images/chop_flix_logo.png'),
+        projectResources: [
+            {
+                id: uuid(),
+                type: 'pic',
+                image: require('../../assets/images/drvmroll.jpg'),
+                websiteLink: 'https://drvmroll-design-portfolio.netlify.app/',
             },
             {
                 id: uuid(),
@@ -25,16 +42,14 @@ export const PORTFOLIO_DATA = [
     {
         id: uuid(),
         index: 2,
-        name: "Project 2",
-        projectCategory: "Graphic Design",
-        urlRep: 'project2.com',
-        projectInfo: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur cum eius pariatur quibusdam quos reiciendis rerum " +
-            "sed ullam velit voluptate? Repellendus, voluptates, voluptatum! Accusamus dicta, ducimus iusto perspiciatis quae temporibus?",
+        projectName: 'Edanra',
+        logo: require('../../assets/images/edanra_logo.png'),
         projectResources: [
             {
                 id: uuid(),
                 type: 'pic',
-                url: ``
+                image: require('../../assets/images/edanra.jpg'),
+                websiteLink: 'https://edanra.com',
             },
             {
                 id: uuid(),
@@ -46,16 +61,14 @@ export const PORTFOLIO_DATA = [
     {
         id: uuid(),
         index: 3,
-        name: "Project 3",
-        projectCategory: "Video Editing",
-        urlRep: 'project3.com',
-        projectInfo: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur cum eius pariatur quibusdam quos reiciendis rerum " +
-            "sed ullam velit voluptate? Repellendus, voluptates, voluptatum! Accusamus dicta, ducimus iusto perspiciatis quae temporibus?",
+        projectName: 'Fr-Logistics',
+        logo: require('../../assets/images/frlogistics_logo.png'),
         projectResources: [
             {
                 id: uuid(),
-                type: 'video',
-                url: ``
+                type: 'pic',
+                image: require('../../assets/images/frlogistics.jpg'),
+                websiteLink: 'https://frlogisticsltd.com/',
             },
             {
                 id: uuid(),
@@ -63,5 +76,5 @@ export const PORTFOLIO_DATA = [
                 url: ``
             }
         ],
-    }
+    },
 ];
