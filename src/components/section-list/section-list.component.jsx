1dda17f097ef5a670d1db6e3497127189709418e
Alfred Asare <alfredasare101@gmail.com>
2020-06-17 19:04:26 +0000

    changed uid to id

--- src/components/section-list/section-list.component.jsx
+++ src/components/section-list/section-list.component.jsx
@@ -5,9 +5,9 @@ import Section from "../project-sections/section.component";
 const SectionList = ({resources}) => {
     return resources ? resources.map((resource, index) => {
         if (resource.type === 'pic'){
-            return <Section type='pic' source={resource.source} key={resource.uid}/>
+            return <Section type='pic' source={resource.source} key={resource.id}/>
         }else if(resource.type === 'video'){
-            return <Section type='video' source={resource.source} key={resource.uid}/>
+            return <Section type='video' source={resource.source} key={resource.id}/>
         }
         return null;
     }) : <></>
