25329a68a8532c668c31fb23dafdfa24498f83c7
Nodenkey <nodenkey@gmail.com>
2020-06-21 10:49:49 +0000

    added fade in

--- src/components/page-animation/page-animation.component.jsx
+++ src/components/page-animation/page-animation.component.jsx
@@ -4,7 +4,10 @@ import {gsap} from "gsap";
 
 const PageTransition = () => {
     let container = useRef(null);
+
     useEffect(() => {
+        const html = document.querySelector('html');
+        html.style.overflowY = 'hidden';
         const tiles = document.querySelectorAll('.tile');
         const fadeIn = () => {
             gsap.to(tiles, {scaleY: 1, transformOrigin: 'left top', duration: 0.7, delay: 1,})
@@ -13,9 +16,11 @@ const PageTransition = () => {
         const fadeOut = () => {
             gsap.to(tiles, {scaleX: 0, transformOrigin: 'right top', duration: 0.7, delay: 1, stagger: .2})
             gsap.to(container, {scaleX: 0, transformOrigin: 'right top', delay: 1.7})
-
         }
         fadeOut();
+        setTimeout(() => {
+            html.style.overflowY = 'scroll';
+        }, 2000)
         return () => {
             fadeIn();
         }
